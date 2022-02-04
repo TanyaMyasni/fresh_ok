e868d648de1c5a603cf3c813345a0e61c4911101
MyasniTanya <myasnitanya@gmail.com>
2022-02-04 18:57:50 +0100

    added offers-slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
-  
-})
+  $('.offers__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt="стрелка влево"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt="стрелка вправо"></button>',
+    fade: true,
+    autoplay: true
+  })
+});
