49182cc803e3b2f5ebf99b7c897b6175e2a4c4cd
MyasniTanya <myasnitanya@gmail.com>
2022-02-11 11:50:41 +0100

    added footer

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,11 @@
 $(function () {
   $('.offers__slider').slick({
-    prevArrow: '<button type="button" class="slick-prev"><img src="images/icons/arrow-left.svg" alt="стрелка влево"></button>',
-    nextArrow: '<button type="button" class="slick-next"><img src="images/icons/arrow-right.svg" alt="стрелка вправо"></button>',
+    prevArrow: $('.offers__btn--prev'),
+    nextArrow: $('.offers__btn--next'),
     fade: true,
-    autoplay: true
+    // autoplay: true
   })
 });
+
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,9 @@ const uglify       = require('gulp-uglify');
 const imagemin     = require('gulp-imagemin');
 const del          = require('del');
 const browserSync  = require('browser-sync').create();
+const svgSprite    = require('gulp-svg-sprite');
+
+
 
 function browsersync() {
   browserSync.init({
@@ -57,11 +60,24 @@ function images() {
     .pipe(dest('dist/images'))
 }
 
+function svgSprites() {
+  return src('app/images/icons/*.svg') 
+    .pipe(
+      svgSprite({
+        mode: {
+          stack: {
+            sprite: '../sprite.svg', 
+          },
+        },
+      })
+    )
+    .pipe(dest('app/images'));
+}
+
 function build() {
   return src([
     'app/*.html',
     'app/css/style.min.css',
-
     'app/js/main.min.js',
   ], {base: 'app'})
   .pipe(dest('dist'))
@@ -73,8 +89,9 @@ function cleanDist() {
 
 function watching() {
   watch(['app/scss/**/*.scss'], styles);
-  watch(['app/js/**/*.js','!app/js/main.min.js'], scripts)
-  watch(['app/**/*.html']).on('change', browserSync.reload)
+  watch(['app/js/**/*.js','!app/js/main.min.js'], scripts);
+  watch(['app/**/*.html']).on('change', browserSync.reload);
+  watch(['app/images/icons/*.svg'], svgSprites);
 }
 
 exports.styles = styles;
@@ -83,6 +100,7 @@ exports.browsersync = browsersync;
 exports.watching = watching;
 exports.images = images;
 exports.cleanDist = cleanDist;
+exports.svgSprites = svgSprites;
 
 exports.build = series(cleanDist, images, build);
-exports.default = parallel(styles, scripts, browsersync, watching); 
+exports.default = parallel(svgSprites, styles, scripts, browsersync, watching);
