ef37938d8697c7beabcc29533fcf2222b2ab72a7
MyasniTanya <myasnitanya@gmail.com>
2022-02-21 21:24:33 +0100

    fixed, edded banners section

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,13 @@
 $(function () {
-  $('.header__select-button').on('click', function () {
-    $('.header__select-menu').toggleClass('.header__select-menu--active');
-    $('.header__select-button').toggleClass('.header__select-button--active');
-    $('.header__content-catalog').toggleClass('.header__content-catalog--active');
+  $('.catalog__btn').on('click', function () {
+    $('.catalog__menu').slideToggle();
+    $('.catalog__btn').toggleClass('catalog__btn--active');
+    $('.header__catalog').toggleClass('header__catalog::after--active');
   });
 
   $('.offers__slider').slick({
-    prevArrow: $('.offers__btn--prev'),
-    nextArrow: $('.offers__btn--next'),
+    prevArrow: $('.offers__slider-btn--prev'),
+    nextArrow: $('.offers__slider-btn--next'),
     fade: true,
     // autoplay: true
   });
