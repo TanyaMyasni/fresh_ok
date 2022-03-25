cce2a645e33f21b4915a6c5186b28a5c296e8b8e
MyasniTanya <myasnitanya@gmail.com>
2022-03-25 12:57:30 +0100

    added promotion section

--- app/js/main.js
+++ app/js/main.js
@@ -12,9 +12,18 @@ $(function () {
     // autoplay: true
   });
 
-  var mixer = mixitup('.top-products__cards');
-  
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
+
+  // var mixer = mixitup('.top-products__cards');
+
 });
-
-
-
