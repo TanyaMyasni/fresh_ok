8fff4ae3aa06e95d624045c30b829977d519e430
MyasniTanya <myasnitanya@gmail.com>
2022-04-10 20:28:08 +0200

    added burger-menu, header adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -20,16 +20,44 @@ $(function () {
   });
 
   $('.user-nav__link--cart').on('click', function () {
-    $('.overlay').toggleClass('is-open');
+    $('.header__overlay').toggleClass('is-open');
     $('.basket__close-btn').addClass('is-open');
     $('body').toggleClass('lock');
   })
 
   $('.basket__close-btn').on('click', function () {
     $('.basket__close-btn').removeClass('is-open');
-    $('.overlay').removeClass('is-open');
+    $('.header__overlay').removeClass('is-open');
     $('body').removeClass('lock');
   })
+
+  $('.header-top__burger-btn').on('click', function () {
+    $('.header-top__overlay').toggleClass('is-open');
+    $('.burger-menu__close-btn').addClass('is-open');
+  })
+
+  $('.burger-menu__close-btn').on('click', function () {
+    $('.header-top__overlay').removeClass('is-open');
+    $('.burger-menu__close-btn').removeClass('is-open');
+  })
+
+  $('.user-nav__link--search').on('click', function () {
+    $('.form').toggleClass('active');
+    $('.form__btn').toggleClass('active');
+  })
+
+  $('.form__btn').on('click', function () {
+    $('.form').removeClass('active');
+    $('.form__btn').removeClass('active');
+  })
+
+  var inp = document.querySelector('.form__input');
+  window.addEventListener('resize', changePlaceholder);
+  changePlaceholder.call(window);
+
+  function changePlaceholder() {
+    inp.setAttribute('placeholder', this.innerWidth >= 992 ? 'Найти в магазине ...' : 'Я ищу...');
+  }
   
 
   var containerEl1 = document.querySelector('[data-ref="container-1"]');
@@ -43,7 +71,5 @@ $(function () {
 
   var mixer1 = mixitup(containerEl1, config);
   var mixer2 = mixitup(containerEl2, config);
-
-  // var mixer = mixitup('.top-products__cards');
 
 });
