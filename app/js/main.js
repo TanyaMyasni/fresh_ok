ab849ff01102feea5de20cae2d33101ce6d28874
MyasniTanya <myasnitanya@gmail.com>
2022-04-25 15:48:20 +0200

    fixed slider, added breadcrumbs

--- app/js/main.js
+++ app/js/main.js
@@ -1,97 +1,132 @@
 $(function () {
-  $('.catalog__btn').on('click', function () {
-    $('.catalog__menu').slideToggle();
-    $('.catalog__btn').toggleClass('catalog__btn--active');
-    $('.header__catalog').toggleClass('header__catalog::after--active');
-  });
+      $('.catalog__btn').on('click', function () {
+        $('.catalog__menu').slideToggle();
+        $('.catalog__btn').toggleClass('catalog__btn--active');
+        $('.header__catalog').toggleClass('header__catalog::after--active');
+      });
 
-  $('.offers__slider').slick({
-    prevArrow: $('.offers__slider-btn--prev'),
-    nextArrow: $('.offers__slider-btn--next'),
-    fade: true,
-    dots: true,
-  });
+      $('.offers__slider').slick({
+          prevArrow: $('.offers__slider-btn--prev'),
+          nextArrow: $('.offers__slider-btn--next'),
+          fade: true,
+          // dots: true,
+          responsive: [{
+              breakpoint: 992,
+              settings: {
+                arrows: false,
+                dots: true,
+              }
+            }],
+          });
 
-  $('.brends-slider__list').slick({
-    arrows: false,
-    infinite: true,
-    slidesToShow: 6,
-    slidesToScroll: 2,
-    responsive: [
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 4,
+        $('.brends-slider__list').slick({
+          arrows: false,
+          infinite: true,
+          slidesToShow: 6,
+          slidesToScroll: 2,
+          responsive: [{
+              breakpoint: 992,
+              settings: {
+                slidesToShow: 4,
+              }
+            },
+            {
+              breakpoint: 786,
+              settings: {
+                slidesToShow: 3,
+              }
+            },
+            {
+              breakpoint: 576,
+              settings: {
+                slidesToShow: 2,
+              }
+            }
+          ]
+        });
+
+        $('.user-nav__link--cart').on('click', function () {
+          $('.header__overlay').toggleClass('is-open');
+          $('.basket__close-btn').addClass('is-open');
+          $('body').toggleClass('lock');
+        })
+
+        $('.basket__close-btn').on('click', function () {
+          $('.basket__close-btn').removeClass('is-open');
+          $('.header__overlay').removeClass('is-open');
+          $('body').removeClass('lock');
+        })
+
+        $('.header-top__burger-btn').on('click', function () {
+          $('.header-top__overlay').toggleClass('is-open');
+          $('.burger-menu__close-btn').addClass('is-open');
+          $('body').toggleClass('lock');
+        })
+
+        $('.burger-menu__close-btn').on('click', function () {
+          $('.header-top__overlay').removeClass('is-open');
+          $('.burger-menu__close-btn').removeClass('is-open');
+          $('body').removeClass('lock');
+        })
+
+        $('.user-nav__link--search').on('click', function () {
+          $('.product-search').toggleClass('active');
+          $('.product-search__btn').toggleClass('active');
+        })
+
+        $('.product-search__btn').on('click', function () {
+          $('.product-search').removeClass('active');
+          $('.product-search__btn').removeClass('active');
+        })
+
+        var inp = document.querySelector('.product-search__input'); window.addEventListener('resize', changePlaceholder); changePlaceholder.call(window);
+
+        function changePlaceholder() {
+          inp.setAttribute('placeholder', this.innerWidth >= 992 ? 'Найти в магазине ...' : 'Я ищу...');
         }
-      },
-      {
-        breakpoint: 786,
-        settings: {
-          slidesToShow: 3,
-        }
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 2,
-        }
-      }
-    ]
-  });
 
-  $('.user-nav__link--cart').on('click', function () {
-    $('.header__overlay').toggleClass('is-open');
-    $('.basket__close-btn').addClass('is-open');
-    $('body').toggleClass('lock');
-  })
+        $('.calculator__minus-btn').on('click', function (e) {
+          e.preventDefault();
+          const $this = $(this);
+          const $input = $this.closest('div').find('input');
+          let value = parseInt($input.val());
 
-  $('.basket__close-btn').on('click', function () {
-    $('.basket__close-btn').removeClass('is-open');
-    $('.header__overlay').removeClass('is-open');
-    $('body').removeClass('lock');
-  })
+          if (value > 1) {
+            value = value - 1;
+          } else {
+            value = 0;
+          }
 
-  $('.header-top__burger-btn').on('click', function () {
-    $('.header-top__overlay').toggleClass('is-open');
-    $('.burger-menu__close-btn').addClass('is-open');
-    $('body').toggleClass('lock');
-  })
+          $input.val(value);
 
-  $('.burger-menu__close-btn').on('click', function () {
-    $('.header-top__overlay').removeClass('is-open');
-    $('.burger-menu__close-btn').removeClass('is-open');
-    $('body').removeClass('lock');
-  })
+        });
 
-  $('.user-nav__link--search').on('click', function () {
-    $('.form').toggleClass('active');
-    $('.form__btn').toggleClass('active');
-  })
+        $('.calculator__plus-btn').on('click', function (e) {
+          e.preventDefault();
+          const $this = $(this);
+          const $input = $this.closest('div').find('input');
+          let value = parseInt($input.val());
 
-  $('.form__btn').on('click', function () {
-    $('.form').removeClass('active');
-    $('.form__btn').removeClass('active');
-  })
+          if (value < 99) {
+            value = value + 1;
+          } else {
+            value = 99;
+          }
 
-  var inp = document.querySelector('.form__input');
-  window.addEventListener('resize', changePlaceholder);
-  changePlaceholder.call(window);
+          $input.val(value);
+        });
 
-  function changePlaceholder() {
-    inp.setAttribute('placeholder', this.innerWidth >= 992 ? 'Найти в магазине ...' : 'Я ищу...');
-  }
-  
 
-  var containerEl1 = document.querySelector('[data-ref="container-1"]');
-  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+        var containerEl1 = document.querySelector('[data-ref="container-1"]');
+        var containerEl2 = document.querySelector('[data-ref="container-2"]');
 
-  var config = {
-    controls: {
-      scope: 'local'
-    }
-  };
+        var config = {
+          controls: {
+            scope: 'local'
+          }
+        };
 
-  var mixer1 = mixitup(containerEl1, config);
-  var mixer2 = mixitup(containerEl2, config);
+        var mixer1 = mixitup(containerEl1, config);
+        var mixer2 = mixitup(containerEl2, config);
 
-});
+      });
