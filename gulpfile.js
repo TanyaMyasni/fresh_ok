e012a166ae35e8320ea53f45fc59c5d59a898d8d
MyasniTanya <myasnitanya@gmail.com>
2022-05-22 15:06:56 +0200

    added product-page product

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,19 @@
-const { src, dest, watch, parallel, series } = require('gulp');
+const {
+  src,
+  dest,
+  watch,
+  parallel,
+  series
+} = require('gulp');
 
-const scss         = require('gulp-sass')(require('sass'));
-const concat       = require('gulp-concat');
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
-const uglify       = require('gulp-uglify');
-const imagemin     = require('gulp-imagemin');
-const del          = require('del');
-const browserSync  = require('browser-sync').create();
-const svgSprite    = require('gulp-svg-sprite');
+const uglify = require('gulp-uglify');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const svgSprite = require('gulp-svg-sprite');
 
 
 
@@ -22,7 +28,9 @@ function browsersync() {
 
 function styles() {
   return src('app/scss/style.scss')
-    .pipe(scss({outputStyle: 'compressed'}))
+    .pipe(scss({
+      outputStyle: 'compressed'
+    }))
     .pipe(concat('style.min.css'))
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 10 versions'],
@@ -34,31 +42,42 @@ function styles() {
 
 function scripts() {
   return src([
-    'node_modules/jquery/dist/jquery.js',
-    'node_modules/slick-carousel/slick/slick.js',
-    'node_modules/swiper/swiper-bundle.js',
-    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
-    'node_modules/mixitup/dist/mixitup.js',
-    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
-    'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
-    'app/js/main.js'
-  ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(dest('app/js'))
-  .pipe(browserSync.stream())
+      'node_modules/jquery/dist/jquery.js',
+      'node_modules/slick-carousel/slick/slick.js',
+      'node_modules/swiper/swiper-bundle.js',
+      'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+      'node_modules/mixitup/dist/mixitup.js',
+      'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+      'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+      'node_modules/rateyo/src/jquery.rateyo.js',
+      'app/js/main.js'
+    ])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'))
+    .pipe(browserSync.stream())
 }
 
 function images() {
   return src('app/images/**/*.*')
     .pipe(imagemin([
-      imagemin.gifsicle({interlaced: true}),
-      imagemin.mozjpeg({quality: 75, progressive: true}),
-      imagemin.optipng({optimizationLevel: 5}),
+      imagemin.gifsicle({
+        interlaced: true
+      }),
+      imagemin.mozjpeg({
+        quality: 75,
+        progressive: true
+      }),
+      imagemin.optipng({
+        optimizationLevel: 5
+      }),
       imagemin.svgo({
-        plugins: [
-          {removeViewBox: true},
-          {cleanupIDs: false}
+        plugins: [{
+            removeViewBox: true
+          },
+          {
+            cleanupIDs: false
+          }
         ]
       })
     ]))
@@ -66,12 +85,12 @@ function images() {
 }
 
 function svgSprites() {
-  return src('app/images/icons/*.svg') 
+  return src('app/images/icons/*.svg')
     .pipe(
       svgSprite({
         mode: {
           stack: {
-            sprite: '../sprite.svg', 
+            sprite: '../sprite.svg',
           },
         },
       })
@@ -81,11 +100,13 @@ function svgSprites() {
 
 function build() {
   return src([
-    'app/*.html',
-    'app/css/style.min.css',
-    'app/js/main.min.js',
-  ], {base: 'app'})
-  .pipe(dest('dist'))
+      'app/*.html',
+      'app/css/style.min.css',
+      'app/js/main.min.js',
+    ], {
+      base: 'app'
+    })
+    .pipe(dest('dist'))
 }
 
 function cleanDist() {
@@ -94,7 +115,7 @@ function cleanDist() {
 
 function watching() {
   watch(['app/scss/**/*.scss'], styles);
-  watch(['app/js/**/*.js','!app/js/main.min.js'], scripts);
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
   watch(['app/**/*.html']).on('change', browserSync.reload);
   watch(['app/images/icons/*.svg'], svgSprites);
 }
